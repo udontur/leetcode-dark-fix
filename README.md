18eec22207b321f8e9e0772444fdf34267c51d5a
udontur <h@hadrian.cc>
2025-06-20 21:19:33 +0800

    feat: home page dark mode

---
+++ README.md
@@ -0,0 +1,2 @@
+# LeetCode Dark Fix
+LeetCode Dark Fix is a userscript that adds dark mode to LeetCode's home page, explore page, and settings page as they dark mode is unavailable even when the site's offical dark mode is turned on. 
--- userscript.js
+++ userscript.js
@@ -7,7 +7,7 @@
 // @include      https://leetcode.com/
 // @include      https://leetcode.com/explore*
 // @include      https://leetcode.com/settings*
-// @description Adds dark mode in LeetCode's home page, explore page, and settings page
+// @description Adds dark mode to LeetCode's home page, explore page, and settings page
 // @license MIT
 // ==/UserScript==
 
@@ -30,6 +30,10 @@ p{
   color: var(--dark-gray-70) !important;
 }
 
+.side-block{
+  border-bottom: 1px solid var(--dark-gray-40) !important;
+}
+
 hr, .side-bar{
   border-top: 1px solid var(--dark-gray-40) !important;
 }
@@ -38,8 +42,62 @@ span {
   color: var(--dark-gray-70) !important;
 }
 
+.storyboard{
+  border-right: 1px solid var(--dark-gray-40) !important;
+}
 .problem-category .problem-category-info a {
   color: var(--dark-gray-80) !important;
+}
+
+.problem-category .problem-category-info a:hover{
+  background-color: var(--dark-gray-40) !important;
+}
+
+#leetcode-navbar{
+  background-color: var(--dark-gray-20) !important;
+}
+
+#leetcode-navbar>div>ul>li>a{
+  color: var(--dark-gray-70) !important;
+}
+
+#leetcode-navbar>div>ul>li>a:hover{
+  color: var(--dark-gray-80) !important;
+}
+.storyboard>div{
+  border-top: 0px solid !important;
+}
+.storyboard>:not(.msg){
+  margin-top: 16px !important;
+}
+svg{
+  color: var(--dark-gray-90) !important;
+}
+
+#subscribe>a>span>span{
+  background-color: #423726 !important;
+  color: #FFA116 !important;
+}
+.side-block-btn{
+  background-color: var(--dark-gray-20) !important;
+  color: var(--dark-gray-80) !important;
+  border-color: var(--dark-gray-40) !important;
+}
+svg>path:nth-child(3){
+  fill: white !important;
+}
+.container__2AdV{
+  border-top: 1px solid var(--dark-gray-40) !important;
+}
+footer{
+  background-color: var(--dark-gray-10) !important;
+}
+.nav-items__1KWV>li>a{
+  color: var(--dark-color-50) !important;
+}
+
+.nav-items__1KWV li:not(:last-child)::after{
+  border-right: var(--dark-color-40) !important;
 }
 `;
 if (typeof GM_addStyle !== "undefined") {
